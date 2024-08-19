cdbf049eeb5ca4306ee1f08bd659e14a1432dfa5
samin <822085977@qq.com>
2024-08-19 16:18:25 +0800

    ✨ Feat: add domain project

---
+++ mbox-domain-system/src/main/java/com/samin/mbox/message/MBoxSystem.java
@@ -0,0 +1,12 @@
+package com.samin.mbox.message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MBoxSystem {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MBoxSystem.class, args);
+    }
+}
---
+++ mbox-domain-system/src/main/java/com/samin/mbox/message/entity/MessageChannel.java
@@ -0,0 +1,4 @@
+package com.samin.mbox.message.entity;
+
+public class MessageChannel {
+}
