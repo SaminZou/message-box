f3f5f1129490bc576db9da10e426dc1c0b933062
samin <822085977@qq.com>
2024-08-19 12:20:05 +0800

    ✨ Feat: enhance message box

---
+++ mbox-application-server/src/main/java/com/samin/mbox/server/config/GlobalExceptionAdvice.java
@@ -0,0 +1,28 @@
+package com.samin.mbox.server.config;
+
+import com.samin.mbox.common.exception.BusiException;
+import com.samin.mbox.common.vo.BaseResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionAdvice {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public BaseResponse<Void> exception(Exception e) {
+        log.error("系统内部错误：", e);
+        return BaseResponse.fail(-1, e.getMessage());
+    }
+
+    @ExceptionHandler(BusiException.class)
+    @ResponseStatus(HttpStatus.OK)
+    public BaseResponse<Void> runtimeException(BusiException e) {
+        log.error("BusException：", e);
+        return BaseResponse.fail(e.getCode(), e.getMessage());
+    }
+}
