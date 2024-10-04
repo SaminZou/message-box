b56b177948f8c15120cb2b78b76bd4402425dc41
samin <822085977@qq.com>
2024-10-04 10:20:49 +0800

    ✨ Feat: enhance message box admin

--- mbox-application/mbox-application-admin/src/main/java/com/samin/mbox/admin/controller/MessageUserController.java
+++ mbox-application/mbox-application-admin/src/main/java/com/samin/mbox/admin/controller/MessageUserController.java
@@ -1,7 +1,17 @@
 package com.samin.mbox.admin.controller;
 
+import com.samin.mbox.common.vo.BaseResponse;
+import com.samin.mbox.common.vo.PageRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/user")
 public class MessageUserController {
+
+    @PostMapping("/page")
+    public BaseResponse<Void> page(PageRequest request) {
+        return BaseResponse.success();
+    }
 }
