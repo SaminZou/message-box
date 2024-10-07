235f29e921d377ae6c0e92ad395f6c1f03d1cf53
SaminZou <822085977@qq.com>
2024-10-07 08:22:13 +0800

    ✨ Feat: enhance message admin

--- mbox-application/mbox-application-admin/src/main/java/com/samin/mbox/admin/controller/MessageLogController.java
+++ mbox-application/mbox-application-admin/src/main/java/com/samin/mbox/admin/controller/MessageLogController.java
@@ -1,4 +1,17 @@
 package com.samin.mbox.admin.controller;
 
+import com.samin.mbox.common.vo.BaseResponse;
+import com.samin.mbox.common.vo.PageRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/log")
 public class MessageLogController {
+
+    @PostMapping("/page")
+    public BaseResponse<Void> page(PageRequest request) {
+        return BaseResponse.success();
+    }
 }
--- mbox-application/mbox-application-admin/src/main/java/com/samin/mbox/admin/controller/MessageMockController.java
+++ mbox-application/mbox-application-admin/src/main/java/com/samin/mbox/admin/controller/MessageMockController.java
@@ -1,5 +1,10 @@
 package com.samin.mbox.admin.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/mock")
 public class MessageMockController {
 
 
